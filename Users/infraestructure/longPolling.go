d90dc13ed68415d03d2e829dff28cd7315f33770
JulioAlb453 <fulioibeto18@gmail.com>
2025-03-28 10:36:14 -0600

    add shor and long polling

---
+++ Users/infraestructure/longPolling.go
@@ -0,0 +1,45 @@
+package infraestructure
+
+import (
+	"net/http"
+	"recu_c1/Users/application"
+	"recu_c1/Users/domain"
+	"sync"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func LongPollingHandler(service *application.UserService)  gin.HandlerFunc{
+	var mu sync.Mutex
+	var clients [] chan []domain.User
+
+	return func (c *gin.Context){
+		client := make(chan []domain.User, 1 )
+		mu.Lock()
+		clients = append(clients, client)
+		mu.Unlock()
+
+		select {
+		case users := <-client:
+			c.JSON(http.StatusOK, users)
+		case <-time.After(1 * time.Second):
+			c.JSON(http.StatusNoContent, nil) // No hay cambios en 1s
+		}
+
+		mu.Lock()
+		for i, ch := range clients {
+			if ch == client {
+				clients = append(clients[:i], clients[i+1:]...)
+				break
+			}
+		}
+		mu.Unlock()
+	}
+}
+
+func NotifyLongPollers(service *application.UserService, clients []chan []domain.User) {
+	for _, client := range clients {
+		client <- service.GetUser()
+	}
+}
